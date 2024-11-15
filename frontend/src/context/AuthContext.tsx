6713320cfd583cd2c315d1489b0e6ec882521b73
yan1451 <yan.brasileiro@gmail.com>
2024-11-15 17:55:57 -0300

    chore(auth-context): criar contexto de autenticação para gerenciar usuário atual

---
+++ frontend/src/context/AuthContext.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { createContext, useContext, useState, ReactNode } from "react";
+
+interface AuthContextType {
+  currentUser: string;
+  setCurrentUser: (value: string) => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState<string>("");
+
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser,
+        setCurrentUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth deve ser usado dentro de um AuthProvider");
+  }
+  return context;
+};
