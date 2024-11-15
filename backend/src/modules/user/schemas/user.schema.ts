b9dbb983e68d47e5ba4c346c2a8552badef805f0
yan1451 <yan.brasileiro@gmail.com>
2024-11-15 18:25:34 -0300

    feat(user) implementa login user dto e user schema

---
+++ backend/src/modules/user/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+import { v4 as uuidv4 } from 'uuid';
+
+export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class User {
+  @Prop({ default: uuidv4 })
+  _id: string;
+
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop()
+  password: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
